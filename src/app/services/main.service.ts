3f4ff3662ce27576e06cec5e2689ffd9558b27a8
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-03-15 13:22:48 +0100

    errorpopup & auth

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -5,6 +5,25 @@ import { Injectable } from '@angular/core';
 })
 export class MainService {
   showPopup = false;
+  errorMessage:string | undefined = 'Heeeey';
 
   constructor() { }
+
+  closePopups(){
+    this.showPopup = false;
+    this.errorMessage = undefined;
+  }
+
+  openPopup(){
+    this.showPopup = true;
+  }
+
+  errorLog(message:string) {
+    this.openPopup();
+    this.errorMessage = message;
+    setTimeout(() => {
+      this.closePopups();
+    }, 3000);
+  }
+
 }
