03f1f3a68b65c5cd8e03749050e7f518dadda735
elidegen <elidegen@icloud.com>
2024-03-15 13:24:22 +0100

    html basic structure

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DummyService } from '../services/dummy.service';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(public ds: DummyService) {
 
+  }
 }
