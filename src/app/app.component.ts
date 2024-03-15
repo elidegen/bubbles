8c231ef039ca5a98039df05132f1a477901c2f22
elidegen <elidegen@icloud.com>
2024-03-15 11:36:31 +0100

    Css Library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainService } from './services/main.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'chatapp';
+  constructor(public ms: MainService){
+
+  }
 }
