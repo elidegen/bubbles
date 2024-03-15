e343dc7ea3a5802cc574069d4f8e9834a81cd0d7
elidegen <elidegen@icloud.com>
2024-03-15 16:00:44 +0100

    new components and library.css update

---
+++ src/app/channel-preview/channel-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './channel-preview.component.html',
+  styleUrl: './channel-preview.component.scss'
+})
+export class ChannelPreviewComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { DummyService } from '../services/dummy.service';
+import { MessageComponent } from '../message/message.component';
+import { ChannelPreviewComponent } from '../channel-preview/channel-preview.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MessageComponent, ChannelPreviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
