8e8e0f9704265d6e876e951a5e1ab83549b81258
elidegen <elidegen@icloud.com>
2024-03-16 17:32:33 +0100

    add ischannel

--- src/app/services/channel.service.ts
+++ src/app/services/channel.service.ts
@@ -1,38 +1,61 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export interface Channel {
   id: number,
   name: string,
   users: number[], // Array of user ID's inside the channel
+  isChannel: boolean
 }
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChannelService {
 
-  channels = [
+  chats = [
     {
       id: 20,
       name: 'testchannel1',
       users: [10, 11, 12, 13, 14],
+      isChannel: true
     },
     {
       id: 21,
       name: 'testchannel2',
       users: [10, 11, 12, 13, 14],
+      isChannel: true
     },
     {
       id: 22,
       name: 'testchannel3',
       users: [10, 11, 12, 13, 14],
+      isChannel: true
+    },
+    {
+      id: 23,
+      name: 'Joshua',
+      users: [10, 11],
+      isChannel: false
+    },
+    {
+      id: 24,
+      name: 'Elijah',
+      users: [10, 11],
+      isChannel: false
+    },
+    {
+      id: 25,
+      name: 'Anna',
+      users: [10, 11],
+      isChannel: false
     }
   ];
+  channels: Channel[];
+  directMessages: Channel[];
 
-  constructor() { }
-
-
+  constructor() { 
+    this.channels = this.chats.filter(obj => obj.isChannel === true);
+    this.directMessages = this.chats.filter(obj => obj.isChannel === false);
+  }
 
 }
