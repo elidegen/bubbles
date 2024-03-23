2820fb9e054e62289ac714c37dbaf0a5fc0c6db1
elidegen <elidegen@icloud.com>
2024-03-23 17:53:40 +0100

    groupedmsg rendering correct and dynamically

--- src/app/grouped-messages/grouped-messages.component.ts
+++ src/app/grouped-messages/grouped-messages.component.ts
@@ -3,6 +3,8 @@ import { Message } from '../services/message.service';
 import { MessageComponent } from '../message/message.component';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-grouped-messages',
@@ -16,9 +18,22 @@ export class GroupedMessagesComponent {
 
   constructor(
     private authService: AuthService,
-  ){}
+    private userService: UserService
+  ) { }
 
-  myMessage(message: Message) {    
+  myMessage(message: Message) {
     return message.author === this.authService.currentUser.id;
+  }
+
+  getAuthor() {
+    const author = this.userService.getUser(this.groupedMessages[0].author);
+    return author
+  }
+
+  getTime() {
+    const lastMsg = this.groupedMessages[this.groupedMessages.length - 1];
+    let date = new Date(lastMsg.created_at)
+    
+    return date
   }
 }
