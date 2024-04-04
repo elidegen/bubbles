95a9ad1a9e3423298f65faf4997f26a39607337a
elidegen <elidegen@icloud.com>
2024-04-04 13:47:26 +0200

    profile file picker working

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -4,11 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MainService {
-  showPopup: boolean = true;
+  showPopup: boolean = false;
   errorMessage: string | undefined;
   loader: boolean = false;
   addChannelPopup: boolean = false;
-  profilePopup: boolean = true;
+  profilePopup: boolean = false;
   addMembersPopup: boolean = false;
   showMembersPopup: boolean = false;
   showEditChannelPopup: boolean = false;
