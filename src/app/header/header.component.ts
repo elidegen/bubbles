a0186a31dbb5239d0fc14618e34bc7c8a5324350
elidegen <elidegen@icloud.com>
2024-04-15 19:23:57 +0200

    theme saved in local storage

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,22 +3,27 @@ import { Component } from '@angular/core';
 import { MainService } from '../services/main.service';
 import { SearchComponent } from '../search/search.component';
 import { AuthService } from '../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, SearchComponent],
+  imports: [CommonModule, SearchComponent, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   openMenu: boolean = false;
   showThemes: boolean = false;
+  selectedTheme: string = localStorage.getItem('selectedTheme') || 'purple';
 
   constructor(
     public mainService: MainService,
     public authService: AuthService,
-    ) { }
+  ) {
+    console.log('selTheme', this.selectedTheme);
+    
+  }
 
   openProfile() {
     this.mainService.showPopup = true;
@@ -29,12 +34,11 @@ export class HeaderComponent {
     this.openMenu = !this.openMenu;
   }
 
-  toggleSideMenu(){
+  toggleSideMenu() {
     this.mainService.sideMenuOpen = !this.mainService.sideMenuOpen;
   }
 
-  setTheme(color: string){
-    console.log('theme');
-    // document.documentElement.style.setProperty('var(--color1)', color)
+  saveTheme() {
+    localStorage.setItem('selectedTheme', this.selectedTheme);
   }
 }
